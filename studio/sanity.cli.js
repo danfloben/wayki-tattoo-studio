716bf9061d748a5b263d54e80f0ba90649287205
danfloben <danfloben@gmail.com>
2026-05-15 12:26:58 -0500

    fix: add sanity.cli.js con projectId para deploy

---
+++ studio/sanity.cli.js
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6gep4uhw',
+    dataset: 'production',
+  },
+  studioHost: 'wayki-tattoo',
+})
